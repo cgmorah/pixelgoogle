d84c801e2a61c625a1a04a3a32b57ec4238d251c
GIOVANNI MORA <cgmorah@gmail.com>
2022-11-20 21:19:49 +1100

    test Front Develop nite9
    Second Challenge

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+/* Template: Google Pixel
+   Author: GIOVANNI MORA
+   Created: Nov 2022
+   Description: Custom JS file
+*/
+
+
+ScrollReveal().reveal('.scright',{
+    delay: 125,
+    duration: 1500,
+    opacity: 0,
+    distance: "50%",
+    origin: "right",
+    reset: true
+  });
+  
+  ScrollReveal().reveal('.scleft',{
+    delay: 125,
+    duration: 1500,
+    opacity: 0,
+    distance: "50%",
+    origin: "left",
+    reset: true
+  });
+  
+  ScrollReveal().reveal('.sctop',{
+    delay: 175,
+    duration: 1500,
+    opacity: 0,
+    distance: "50%",
+    origin: "top",
+    reset: true
+  });
+
+  ScrollReveal().reveal(".fade-in-1", {
+    duration: 1500,
+    move: 0,
+    reset: true
+  });
+
+  ScrollReveal().reveal(".fade-in-2", {
+    duration: 3000,
+    move: 0,
+    reset: true
+  });
+
+  ScrollReveal().reveal(".fade-in-3", {
+    duration: 5000,
+    move: 0,
+    reset: true
+  });
+
+  ScrollReveal().reveal(".scaleUp", {
+    duration: 3000,
+    scale: 0.75,
+  });
